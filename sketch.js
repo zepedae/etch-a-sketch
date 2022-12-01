a5cbef2c1155800ac4772054445e854330743e42
Emily Zepeda <emilyazepeda@gmail.com>
2022-12-01 14:01:13 -0600

    Pixels get darker with each mouseover

--- sketch.js
+++ sketch.js
@@ -21,16 +21,27 @@ document.querySelector('#create').addEventListener('click', () => {
     
     document.querySelectorAll('.row').forEach(e => e.remove());
 
-    let length;
-    while(!length || length > 100 || length < 0){
-        length = prompt('Enter grid length (<100)');
-    }
+    let length = 10;
+    // while(!length || length > 100 || length < 0){
+    //     length = prompt('Enter grid length (<100)');
+    // }
     createGrid(length);
     
     const pixels = document.querySelectorAll('.pixel');
+
     pixels.forEach(pixel => pixel.addEventListener('mouseover', () => {
-        pixel.classList.add('sketched');
+        if(!pixel.style.backgroundColor){
+            pixel.style.backgroundColor = 'rgb(230,230,250)';
+        } else {
+            let colorValues = pixel.style.backgroundColor.split(',');
+            colorValues[0] = Number(colorValues[0].substr(4)) - 20;
+            colorValues[1] = Number(colorValues[1]) - 20;
+            colorValues[2] = Number(colorValues[2].slice(0, -1)) - 20;
+
+            pixel.style.backgroundColor = `rgb(${colorValues})`;
+
+            console.log(`rgb(${colorValues})`);
+        }
     }));
-    
 });
 
